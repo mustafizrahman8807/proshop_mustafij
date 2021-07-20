bd45ba98a19f6ae0d8731139516da75023be25d3
Mustafijur Rahman <mustafizrahman8807@gmail.com>
2021-07-20 12:17:26 +0600

    First commit

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
+import Rating from '../component/Rating';
+import products from '../products';
 
 const Product = () => {
   return (
       <div>
-        Product
+        <h1>Product</h1>
       </div>
   );
 };
