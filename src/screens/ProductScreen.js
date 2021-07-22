b7916cff3cf0eef3a584a8562e7531423abb9441
Mustafijur Rahman <mustafizrahman8807@gmail.com>
2021-07-22 09:39:46 +0600

    Third commit

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -4,11 +4,58 @@ import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from '../component/Rating';
 import products from '../products';
 
-const Product = () => {
+const Product = ({match}) => {
+  const product = products.find((p) => p._id === match.params.id);
+
   return (
-      <div>
-        <h1>Product</h1>
-      </div>
+      <>
+        <Row>
+          <Col md={6} className='py-3'>
+            <Image src={product.image} alt={product.name} fluid/>
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                    value={product.rating}
+                    text={`${product.rating} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='fluid'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className='btn-block w-100' type='button' disabled={product.countInStock === 0}>
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+        <Link className='btn btn-dark py-3 rounded' to='/'>Go Back</Link>
+      </>
   );
 };
 
